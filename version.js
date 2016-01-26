aef1eef4f009eb6d6cd7fc199c9a9dd928fee894
Eduardo Noyer <e.noyer.silva@gmail.com>
2016-01-26 11:21:19 -0600

    Update version.js

--- version.js
+++ version.js
@@ -2,5 +2,5 @@
   FileName : "PebbleWuff-10_1.0.1.0_arm.appxbundle",
   VersionNumber : "0.0.2",
   Release : "alpha",
-  Url : "https://github.com/bitrvmpd/Pebble-W10M/releases/download/v0.0.2-alpha/PebbleWuff-10_1.0.1.0_arm.appxbundle"
+  Url : "https://github.com/bitrvmpd/Pebble-W10M/releases/download/v0.0.2-alpha/PebbleWuff-10_1.0.1.0_arm.appxbundle.zip"
 }
